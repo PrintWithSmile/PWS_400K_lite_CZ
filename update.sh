b7b85eafedcc06cb5de6dedbeea478b91a36ceed
Print With Smile <123603173+PrintWithSmile@users.noreply.github.com>
2023-11-20 10:37:36 +0100

    v0.1.3

--- update.sh
+++ update.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+if ! command -v zip &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install zip -y
+
+    if [ $? -eq 0 ]; then
+        echo "ZIP utility installed successfully."
+    else
+        echo "Failed to install ZIP utility."
+        exit 1
+    fi
+fi
+
+slozka="/home/pi/Klipper_IP"
+
+if [ ! -d "$slozka" ]; then
+    git clone https://github.com/PrintWithSmile/Klipper_IP.git
+	cd Klipper_IP
+	chmod +x install.sh
+	./install.sh
+fi
+
+
 echo "Zálohuji předchozí konfigurace"
 cd /home/pi/printer_data/config
 zip -r "zaloha_$(date +"%d-%m-%Y").zip" /home/pi/printer_data/config/* -x "/home/pi/printer_data/config/Archive/*" -x "/home/pi/printer_data/config/Archive"
